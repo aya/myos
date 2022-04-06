d328ae3e24d67d3f1442b36ee040d40f76bf34f5
Yann Autissier <yann.autissier@gmail.com>
2022-04-06 16:17:51 +0200

    fetch user ssh public key from gitlab

--- docker/x2go/xfce-debian/setup_users.sh
+++ docker/x2go/xfce-debian/setup_users.sh
@@ -5,8 +5,10 @@ for user in ${USERS:-${USERNAME}}; do
   id ${user} > /dev/null 2>&1 || useradd -ms /bin/bash ${user}
   usermod -a -G x2gouser ${user}
   mkdir -p /home/${user}/.ssh
-  wget -qO /home/${user}/.ssh/authorized_keys https://github.com/${user}.keys
+  wget -qO /home/${user}/.ssh/authorized_keys https://gitlab.com/${user}.keys 2>/dev/null \
+   || wget -qO /home/${user}/.ssh/authorized_keys https://github.com/${user}.keys 2>/dev/null
   chown -R ${user} /home/${user}/.ssh
+  chmod 0750 /home/${user}
 done
 for sudoer in ${SUDOERS:-}; do
   usermod -a -G sudo ${sudoer}
